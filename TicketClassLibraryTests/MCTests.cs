17b374ca45b1536519df47bc2a5ce7f6b6dbf2b0
Frederik Rasmussen <wtfhaxxorone@gmail.com>
2024-09-26 12:06:50 +0200

    Added MC and tests for MC

---
+++ TicketClassLibraryTests/MCTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicketClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketClassLibrary.Tests
+{
+    [TestClass()]
+    public class MCTests
+    {
+        [TestMethod()]
+        public void PriceTest()
+        {
+            MC testSubject = new();
+
+            Assert.AreEqual(125.0, testSubject.Price());
+        }
+
+        [TestMethod()]
+        public void VehicleTypeTest()
+        {
+            MC testSubject = new();
+
+            Assert.AreEqual("MC", testSubject.Vehicle());
+        }
+    }
+}

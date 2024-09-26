73cfe233dfd65bb35a82890121a313200f99d373
Frederik Rasmussen <wtfhaxxorone@gmail.com>
2024-09-26 12:04:00 +0200

    Added Car and tests for car functions.

---
+++ TicketClassLibrary/Car.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketClassLibrary
+{
+    public class Car
+    {
+        /// <summary>
+        /// String containing the license plate of a vehicle.
+        /// </summary>
+        public string LicensePlate {  get; set; }
+
+        /// <summary>
+        /// Date of the given fare.
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Function to check the price for a vehicles fare
+        /// </summary>
+        /// <returns>Double with the price of the fare.</returns>
+        public double Price()
+        {
+            return 240.0;
+        }
+
+        /// <summary>
+        /// Function to check the type of vehicle crossing the bridge.
+        /// </summary>
+        /// <returns>String with description of vehicles type.</returns>
+        public string VehicleType()
+        {
+            return "Car";
+        }
+    }
+}
---
+++ TicketClassLibraryTests/CarTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicketClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketClassLibrary.Tests
+{
+    [TestClass()]
+    public class CarTests
+    {
+        [TestMethod()]
+        public void PriceTest()
+        {
+            Car testSubject = new();
+
+            Assert.AreEqual(240.0, testSubject.Price());
+        }
+
+        [TestMethod()]
+        public void VehicleTypeTest()
+        {
+            Car testSubject = new();
+
+            Assert.AreEqual("Car", testSubject.VehicleType());
+        }
+    }
+}
